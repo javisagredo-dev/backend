3e6ecff351ee248068e9394c82438e4bf713d9c8
Javier Sagredo <javi.sagredo@duocuc.cl>
2025-11-14 12:19:59 -0300

    Inicio backend react

---
+++ src/main/java/cl/duocuc/huertohogar/backend/HuertoHogarBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.duocuc.huertohogar.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuertoHogarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuertoHogarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duocuc/huertohogar/backend/HuertoHogarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duocuc.huertohogar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuertoHogarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
