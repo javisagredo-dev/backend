001950348ad4ea0031448b4efccec8a77f2ccf14
khrixtar-dev <khrixtar.gg@gmail.com>
2025-11-27 20:22:14 -0300

    api de tickets agregada

--- src/main/java/cl/duocuc/huertohogar/backend/dto/TicketCreateRequestDTO.java
+++ src/main/java/cl/duocuc/huertohogar/backend/dto/TicketCreateRequestDTO.java
@@ -1,10 +1,10 @@
 package cl.duocuc.huertohogar.backend.dto;
 
-import lombok.Data;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import lombok.Data;
 // Es el request completo para crear un ticket (la compra)
 // Es el body del POST /api/tickets
 // Lo recibe el controller
@@ -14,6 +14,6 @@ public class TicketCreateRequestDTO {
 
     private Long userId; 
     private List<TicketCreateItemDTO> items;
-    private BigDecimal total;        // Total calculado en el front
+    private BigDecimal total;            // Total calculado en el front
     private LocalDateTime purchaseDate;  // Fecha desde el front
 }
