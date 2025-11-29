e4f26d8e074470e64dbd394615672317213c69c3
javisagredo-dev <javi.sagredo@duocuc.cl>
2025-11-29 19:35:58 -0300

    se agrego un detalle con cross origin al login y registro

--- src/main/java/cl/duocuc/huertohogar/backend/security/controller/AuthenticationController.java
+++ src/main/java/cl/duocuc/huertohogar/backend/security/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import cl.duocuc.huertohogar.backend.security.auth.AuthenticationService;
 import cl.duocuc.huertohogar.backend.security.auth.RegisterRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthenticationController {
 
     private final AuthenticationService service;
